eb2289d5d2a06da372bd934d8f5e3278cf36b933
Joshua Granick <joshua@joshuagranick.com>
2013-11-20 13:36:41 -0800

    ErrorEvent fix

--- flash/events/ErrorEvent.hx
+++ flash/events/ErrorEvent.hx
@@ -6,11 +6,14 @@ class ErrorEvent extends TextEvent {
 	
 	public static var ERROR:String = "error";
 	
+	public var errorID (default, null):Int;
 	
-	public function new (type:String, ?bubbles:Bool, ?cancelable:Bool, ?text:String):Void {
+	
+	public function new (type:String, ?bubbles:Bool, ?cancelable:Bool, ?text:String, id:Int = 0):Void {
 		
 		super (type, bubbles, cancelable);
 		this.text = text;
+		errorID = id;
 		
 	}
 	
